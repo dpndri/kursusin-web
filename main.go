1a1c3cdfb11b429813b03680efac04840d6cf36b
dpndri <irgidapiandri@gmail.com>
2026-06-05 15:30:10 +0700

    prepare deployment

--- main.go
+++ main.go
@@ -81,7 +81,12 @@ func main() {
 
 	r.GET("/statistik", statistik)
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run("0.0.0.0:" + port)
 }
 
 func mustLoadData() {
